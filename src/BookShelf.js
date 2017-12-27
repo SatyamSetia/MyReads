fba789130059c5146caa3a32633911899314043c
Satyam <satyam7991@gmail.com>
2017-12-27 11:02:54 +0530

    feat: shelf update completed

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -9,34 +9,53 @@ class BookShelf extends Component {
 	};
 
 	componentDidMount() {
-		BooksAPI.getAll().then(books =>
-			this.setState({
-				books: books.filter(book => book.shelf === this.props.shelf)
-			},() => this.setState({loading: false}))
-		);
+		setInterval(() => {
+			this.setState(() => {
+				BooksAPI.getAll().then(books =>
+					this.setState(
+						{
+							books: books.filter(
+								book => book.shelf === this.props.shelf
+							)
+						},
+						() => this.setState({ loading: false })
+					)
+				);
+			});
+		}, 1000);
 	}
+
+	shouldComponentUpdate(nextProps,nextState) {
+		console.log(this.state.books !== nextState.books)
+        return this.state.books !== nextState.books;
+    }
 
 	loader() {
 		return <div>Loading...</div>;
 	}
 
 	emptyShelf() {
-		return <div>Shelf is empty.</div>
+		return <div>Shelf is empty.</div>;
 	}
 
 	render() {
+		console.log('render');
 		return (
 			<div className="bookshelf">
 				<h2 className="bookshelf-title">{this.props.title}</h2>
 				<div className="bookshelf-books">
 					<ol className="books-grid">
-						{this.state.loading?this.loader():this.state.books.length===0?this.emptyShelf():this.state.books.map(book => {
-							return (
-								<li key={book.id}>
-									<BookDetail book={book} />
-								</li>
-							);
-						})}
+						{this.state.loading
+							? this.loader()
+							: this.state.books.length === 0
+								? this.emptyShelf()
+								: this.state.books.map(book => {
+										return (
+											<li key={book.id}>
+												<BookDetail book={book} />
+											</li>
+										);
+									})}
 					</ol>
 				</div>
 			</div>
